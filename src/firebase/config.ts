d144b39f675540bc126550816a4d8a68b611ab6c
Abhishek Lodha <abhisheklodha13@gmail.com>
2025-12-11 07:57:02 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-9641115154-a8776",
-  "appId": "1:834536673928:web:7c5e083f1eeb8282ad053d",
-  "apiKey": "AIzaSyBNV4AdSSRst1wbu0yVpk7pxGIWzuKm5CU",
-  "authDomain": "studio-9641115154-a8776.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "834536673928"
+  "projectId": "codeprep-a08f0",
+  "appId": "1:66494916522:web:a62b32b842a2202619b015",
+  "apiKey": "AIzaSyAh9C_u3_89hS4A8fJ2kP3s5t8YxVOE2p8",
+  "authDomain": "codeprep-a08f0.firebaseapp.com",
+  "storageBucket": "codeprep-a08f0.appspot.com",
+  "messagingSenderId": "66494916522"
 };
